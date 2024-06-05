6be36ab3822a235d60a51bc98e5746af72f8a671
Rahul Gupta <rg5353070@gmail.com>
2024-06-05 21:31:40 +0530

    fixed the pagination for blogs section and added a couple of pages to the site

--- slices/TextBlock/index.tsx
+++ slices/TextBlock/index.tsx
@@ -1,5 +1,10 @@
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+
+// Then register the languages you need
+hljs.registerLanguage("javascript", javascript);
 
 /**
  * Props for `TextBlock`.
@@ -15,7 +20,7 @@ const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="prose prose-base md:prose-lg prose-code:text-yellow-200 dark:prose-invert max-w-none w-full">
+      <div className="prose prose-base md:prose-lg prose-code:text-yellow-200 dark:prose-invert max-w-none w-full prose-code:language-javascript">
         <PrismicRichText field={slice.primary.content} />
       </div>
     </section>
